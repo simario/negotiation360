f98a9026d2b4770e9e78d34eb0fd6c0bc97fd40e
trenholm9 <trenholm@me.com>
2014-05-19 15:19:44 -0400

    Content edits

--- Negotiation3/reports/charts-examples.js
+++ Negotiation3/reports/charts-examples.js
@@ -241,7 +241,7 @@ $(function () {
         series: [{
             name: 'Rating',
             type: 'area',
-            data: [5, 5, 5, 5],
+            data: [4, 4, 4, 4],
 			lineWidth: 5,
             animation: {
                 duration: 4000
@@ -324,7 +324,7 @@ $(function () {
         series: [{
             name: 'Rating',
             type: 'area',
-            data: [7, 7, 3, 3],
+            data: [6, 6, 2, 2],
 			lineWidth: 5,
             animation: {
                 duration: 4000
@@ -409,7 +409,177 @@ $(function () {
         series: [{
             name: 'Rating',
             type: 'area',
-            data: [3, 3, 7, 7],
+            data: [2, 2, 6, 6],
+			lineWidth: 5,
+            animation: {
+                duration: 4000
+            }
+        }]
+
+    });
+});		
+
+$(function () {
+
+    $('#superiorRelational').highcharts({
+
+        chart: {
+            polar: true,
+            borderColor: '#34aadc',
+            borderWidth: 2,
+            type: 'line',
+            animation: {
+                duration: 2000
+            }
+        },
+
+        legend: {
+            enabled: false
+        },
+
+
+        title: {
+            text: "Title to Come",
+	        margin: 15
+        },
+
+        pane: {
+            size: '80%',
+	        startAngle: -45,
+        },
+
+        xAxis: {
+            categories: ['Creating Value', 'Empathy', 'Claiming Value', 'Assert'],
+            tickmarkPlacement: 'on',
+            lineWidth: 0,
+			labels: {
+				style: {
+					whiteSpace: 'nowrap'
+				}
+			}
+        },
+
+        yAxis: {
+            lineWidth: 0,
+			tickInterval: 4,
+            max: 8,
+			min: 0
+        },
+
+        tooltip: {
+            enabled: false
+        },
+
+		credits: {
+            enabled: false
+         },
+
+        navigation: {
+            buttonOptions: {
+                enabled: false
+            }
+        },
+
+        plotOptions: {
+            series: {
+                fillOpacity: 0.33,
+                marker: {
+                    fillColor: '#FFFFFF',
+                    lineWidth: 2,
+                    lineColor: null // inherit from series
+                }
+            },
+        },
+
+        series: [{
+            name: 'Rating',
+            type: 'area',
+            data: [2, 6, 2, 6],
+			lineWidth: 5,
+            animation: {
+                duration: 4000
+            }
+        }]
+
+    });
+});		
+
+$(function () {
+
+    $('#superiorOutcomes').highcharts({
+
+        chart: {
+            polar: true,
+            borderColor: '#34aadc',
+            borderWidth: 2,
+            type: 'line',
+            animation: {
+                duration: 2000
+            }
+        },
+
+        legend: {
+            enabled: false
+        },
+
+
+        title: {
+            text: "Title to Come",
+	        margin: 15
+        },
+
+        pane: {
+            size: '80%',
+	        startAngle: -45,
+        },
+
+        xAxis: {
+            categories: ['Creating Value', 'Empathy', 'Claiming Value', 'Assert'],
+            tickmarkPlacement: 'on',
+            lineWidth: 0,
+			labels: {
+				style: {
+					whiteSpace: 'nowrap'
+				}
+			}
+        },
+
+        yAxis: {
+            lineWidth: 0,
+			tickInterval: 4,
+            max: 8,
+			min: 0
+        },
+
+        tooltip: {
+            enabled: false
+        },
+
+		credits: {
+            enabled: false
+         },
+
+        navigation: {
+            buttonOptions: {
+                enabled: false
+            }
+        },
+
+        plotOptions: {
+            series: {
+                fillOpacity: 0.33,
+                marker: {
+                    fillColor: '#FFFFFF',
+                    lineWidth: 2,
+                    lineColor: null // inherit from series
+                }
+            },
+        },
+
+        series: [{
+            name: 'Rating',
+            type: 'area',
+            data: [6, 2, 6, 2],
 			lineWidth: 5,
             animation: {
                 duration: 4000
