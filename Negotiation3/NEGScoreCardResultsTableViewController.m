5a7cd8f809b2f18e4a546ba6d427edc77cd62907
trenholm9 <trenholm@me.com>
2014-10-05 14:06:26 -0400

    Added circle js for results style

--- Negotiation3/NEGScoreCardResultsTableViewController.m
+++ Negotiation3/NEGScoreCardResultsTableViewController.m
@@ -93,7 +93,7 @@
     
     
     
-    NSString *finalString = [NSString stringWithFormat:htmlString, reachAgreement, typeOfNeg, importance, satisfaction];
+    NSString *finalString = [NSString stringWithFormat:htmlString, reachAgreement, typeOfNeg, importance, importance, satisfaction, satisfaction];
     NSURL *url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@/", [[NSBundle mainBundle] bundlePath]]];
     
     UIWebView *webView = (UIWebView *)[cell viewWithTag:999];
