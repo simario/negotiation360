8a9ecda7f2b1cfd711095b5c3fd87a81253fd296
Curtis Hermann <chermann@roundarchisobar.com>
2014-11-04 19:44:13 -0600

    complete check for best practices

--- Negotiation3/NEGBestPracticesWWWViewController.m
+++ Negotiation3/NEGBestPracticesWWWViewController.m
@@ -107,6 +107,8 @@
     
     [fetchRequest setSortDescriptors:sortDescriptors];
     
+    [fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"complete == YES"]];
+    
     // Edit the section name key path and cache name if appropriate.
     // nil for section name key path means "no sections".
     NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.managedObjectContext sectionNameKeyPath:nil cacheName:@"Master"];
